7d6b3f47196bd09c2b613e1aedefd7445baf0b4e
Marcel Zięba <marseel@gmail.com>
2015-01-13 22:28:30 +0100

    Error handling

--- sandbox/runner.R
+++ sandbox/runner.R
@@ -4,4 +4,9 @@ source(paste(args[[2]], "init.R", sep=""))
 source(paste(args[[2]], "masked.functions.R", sep=""))
 source(paste(args[[2]], "sandbox.R", sep=""))
 src <- readChar(args[[1]], file.info(args[[1]])$size)
-sandbox(src)
+res <- tryCatch(sandbox(src), error = function(e) e)
+
+if (any(class(res) == 'error')) {
+    print('FAIL');
+    print(res[[1]]);
+}
