f7075554da9bc23e1c8c5d9802433f04f4f55d0e
Marcel Zięba <marseel@gmail.com>
2015-01-17 02:03:30 +0100

    All throught runner

--- sandbox/runner.R
+++ sandbox/runner.R
@@ -1,21 +1,26 @@
 args <- commandArgs(trailingOnly = TRUE)
-source(paste(args[[2]], "blacklist.R", sep=""))
-source(paste(args[[2]], "init.R", sep=""))
-source(paste(args[[2]], "masked.functions.R", sep=""))
-source(paste(args[[2]], "sandbox.R", sep=""))
+sand <- args[[2]]
 
 size <- nchar(args[[1]])
 prefix <- substr(args[[1]], 1, size - 2)
 img <- paste(prefix, "png", sep="")
 png(img)
 
-src <- readChar(args[[1]], file.info(args[[1]])$size)
-res <- tryCatch(sandbox(src), error = function(e) e)
+if (args[[3]] == "1") {
+    source(paste(args[[2]], "blacklist.R", sep=""))
+    source(paste(args[[2]], "init.R", sep=""))
+    source(paste(args[[2]], "masked.functions.R", sep=""))
+    source(paste(args[[2]], "sandbox.R", sep=""))
+    src <- readChar(args[[1]], file.info(args[[1]])$size)
+    res <- tryCatch(sandbox(src), error = function(e) e)
+
+    if (any(class(res) == 'error')) {
+        print('FAIL');
+        print(res[[1]]);
+    }
+} else {
+    source(args[[1]]);
+}
 
 img.exists <- !is.na(file.info(img)$size)
 off <- dev.off()
-
-if (any(class(res) == 'error')) {
-    print('FAIL');
-    print(res[[1]]);
-}
